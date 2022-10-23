0e9d35995cb9f543168c75cef675dbee0a877742
prabhu <prabhu_tc@yahoo.com>
2022-10-23 14:49:07 +0530

    [Adding EmployeeServiceImpl.java]

---
+++ src/main/java/com/gl/empmgnt/service/EmployeeServiceImpl.java
@@ -0,0 +1,36 @@
+package com.gl.empmgnt.service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gl.empmgnt.model.Employee;
+import com.gl.empmgnt.repository.EmployeeRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class EmployeeServiceImpl implements EmployeeService {
+
+	@Autowired
+	private EmployeeRepository employeeRepository;
+
+	public Employee saveEmployee(Employee employee) {
+		return this.employeeRepository.save(employee);
+	}
+
+	public Set<Employee> fetchAllEmployees() {
+		return new HashSet<>(this.employeeRepository.findAll());
+	}
+
+	public Employee fetchEmployeeById(long empId) {
+		return this.employeeRepository.findById(empId).orElseThrow();
+	}
+
+	public void deleteEmployeeById(long empId) {
+		this.employeeRepository.deleteById(empId);
+	}
+}
