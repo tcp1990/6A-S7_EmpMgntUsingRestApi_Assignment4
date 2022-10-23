173b7a42e19436558bb28a700133582b590329e6
prabhu <prabhu_tc@yahoo.com>
2022-10-23 15:34:50 +0530

    [Adding BootstrapAppData.java]

---
+++ src/main/java/com/gl/empmgnt/util/BootstrapAppData.java
@@ -0,0 +1,39 @@
+package com.gl.empmgnt.util;
+
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
+
+import com.gl.empmgnt.model.Employee;
+import com.gl.empmgnt.repository.EmployeeRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class BootstrapAppData {
+
+	private final EmployeeRepository employeeRepository;
+	
+	@EventListener(ApplicationReadyEvent.class)
+	public void insertEmployees(ApplicationReadyEvent event) {
+		
+		Employee dhineshEmpObj = new Employee();
+		dhineshEmpObj.setFirstName("DhineshBabu");
+		dhineshEmpObj.setLastName("Selvam");
+		dhineshEmpObj.setEmailAddress("dhineshbabu@gmail.com");
+		this.employeeRepository.save(dhineshEmpObj);
+		
+		Employee anandEmpObj = new Employee();
+		anandEmpObj.setFirstName("AnandKumar");
+		anandEmpObj.setLastName("Mani");
+		anandEmpObj.setEmailAddress("anandkumar@gmail.com");
+		this.employeeRepository.save(anandEmpObj);
+		
+		Employee samEmpObj = new Employee();
+		samEmpObj.setFirstName("SamNithyananth");
+		samEmpObj.setLastName("Chandran");
+		samEmpObj.setEmailAddress("samnithyananth@gmail.com");
+		this.employeeRepository.save(samEmpObj);
+	}
+}
