0ce14cf8f9fffffe7c37277d52b81f97bd6a6a0f
prabhu <prabhu_tc@yahoo.com>
2022-10-23 15:07:41 +0530

    [Deleting Data.sql]

--- src/main/resources/Data.sql
+++
@@ -1,5 +0,0 @@
--- Insert data into EMPLOYEES Table
-INSERT INTO employees (id, first_name, last_name, email_address) VALUES (1, 'DhineshBabu', 'Selvam', 'dhineshbabu@gmail.com');
-INSERT INTO employees (id, first_name, last_name, email_address) VALUES (2, 'AnandKumar', 'Mani', 'anandkumar@gmail.com');
-INSERT INTO employees (id, first_name, last_name, email_address) VALUES (3, 'SamNithyananth', 'Chandran', 'samnithyananth@gmail.com');
-INSERT INTO employees (id, first_name, last_name, email_address) VALUES (4, 'Karthick', 'Raja', 'karthickraja@gmail.com');
