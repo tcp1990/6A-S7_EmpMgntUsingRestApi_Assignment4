270d6613ff421145c1234c6a7d387a24c85fcf5f
prabhu <prabhu_tc@yahoo.com>
2022-10-23 14:39:37 +0530

    [Adding EmployeeRepository.java]

---
+++ src/main/java/com/gl/empmgnt/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.gl.empmgnt.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.gl.empmgnt.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
