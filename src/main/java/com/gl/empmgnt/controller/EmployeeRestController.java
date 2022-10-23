c63dbc51536b088ce6f6ec1fde95230b194a7133
prabhu <prabhu_tc@yahoo.com>
2022-10-23 15:03:15 +0530

    [Adding EmployeeRestController.java]

---
+++ src/main/java/com/gl/empmgnt/controller/EmployeeRestController.java
@@ -0,0 +1,45 @@
+package com.gl.empmgnt.controller;
+
+import java.util.Set;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gl.empmgnt.model.Employee;
+import com.gl.empmgnt.service.EmployeeService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/employees")
+@RequiredArgsConstructor
+public class EmployeeRestController {
+
+	private final EmployeeService employeeService;
+
+	@PostMapping
+	public Employee saveEmployee(@RequestBody Employee employee) {
+		return this.employeeService.saveEmployee(employee);
+	}
+
+	@GetMapping
+	public Set<Employee> fetchAllEmployees() {
+		return this.employeeService.fetchAllEmployees();
+	}
+
+	@GetMapping("/{id}")
+	public Employee fetchEmployeeById(@PathVariable("id") long empId) {
+		return this.employeeService.fetchEmployeeById(empId);
+	}
+
+	@DeleteMapping("/{id}")
+	public void deleteEmployeeById(@PathVariable("id") long empId) {
+		this.employeeService.deleteEmployeeById(empId);
+	}
+
+}
