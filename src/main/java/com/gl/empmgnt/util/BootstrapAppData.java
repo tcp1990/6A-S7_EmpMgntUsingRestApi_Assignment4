259ba7323b436f10c3877cc70015ff516be855a5
prabhu <prabhu_tc@yahoo.com>
2022-10-23 16:35:07 +0530

    [Updating BootstrapAppData.java]

--- src/main/java/com/gl/empmgnt/util/BootstrapAppData.java
+++ src/main/java/com/gl/empmgnt/util/BootstrapAppData.java
@@ -17,51 +17,57 @@ import lombok.RequiredArgsConstructor;
 public class BootstrapAppData {
 
 	private final EmployeeRepository employeeRepository;
+
 	private final UserRepository userRepository;
-	
+
 	@EventListener(ApplicationReadyEvent.class)
 	public void insertEmployees(ApplicationReadyEvent event) {
-		
+
 		Employee dhineshEmpObj = new Employee();
 		dhineshEmpObj.setFirstName("DhineshBabu");
 		dhineshEmpObj.setLastName("Selvam");
 		dhineshEmpObj.setEmailAddress("dhineshbabu@gmail.com");
 		this.employeeRepository.save(dhineshEmpObj);
-		
+
 		Employee anandEmpObj = new Employee();
 		anandEmpObj.setFirstName("AnandKumar");
 		anandEmpObj.setLastName("Mani");
 		anandEmpObj.setEmailAddress("anandkumar@gmail.com");
 		this.employeeRepository.save(anandEmpObj);
-		
+
 		Employee samEmpObj = new Employee();
 		samEmpObj.setFirstName("SamNithyananth");
 		samEmpObj.setLastName("Chandran");
 		samEmpObj.setEmailAddress("samnithyananth@gmail.com");
 		this.employeeRepository.save(samEmpObj);
-		
+
 		Employee karthickEmpObj = new Employee();
 		karthickEmpObj.setFirstName("KarthickRaja");
 		karthickEmpObj.setLastName("Ramasamy");
 		karthickEmpObj.setEmailAddress("karthickraja@gmail.com");
 		this.employeeRepository.save(karthickEmpObj);
-		
+
+	}
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void insertUsersAndRoles(ApplicationReadyEvent event) {
+
 		Role userRole = new Role();
 		userRole.setRoleName("USER");
-		
+
 		Role adminRole = new Role();
 		adminRole.setRoleName("ADMIN");
-		
+
 		Role managerRole = new Role();
 		managerRole.setRoleName("MANAGER");
-		
+
 		User vinayUserObj = new User();
 		vinayUserObj.setUsername("vinay");
 		vinayUserObj.setPassword("welcome");
 		vinayUserObj.setEmailAddress("vinay@gmail.com");
 		vinayUserObj.addRole(adminRole);
 		this.userRepository.save(vinayUserObj);
-		
+
 		User rakeshUserObj = new User();
 		rakeshUserObj.setUsername("rakesh");
 		rakeshUserObj.setPassword("welcome");
